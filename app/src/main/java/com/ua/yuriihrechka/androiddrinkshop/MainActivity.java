75fd37f67fba2448de9aa8b045b169a10027e4dd
ukropBandera <ukrop.bandera@gmail.com>
2019-03-26 23:36:46 +0200

    1_7

--- app/src/main/java/com/ua/yuriihrechka/androiddrinkshop/MainActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/androiddrinkshop/MainActivity.java
@@ -1,22 +1,39 @@
 package com.ua.yuriihrechka.androiddrinkshop;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import dmax.dialog.SpotsDialog;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1000;
     private Button btn_continue;
 
     @Override
@@ -26,11 +43,71 @@ public class MainActivity extends AppCompatActivity {
 
 
         btn_continue = (Button)findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLoginPage(LoginType.PHONE);
+            }
+        });
 
         //printKeyHash();
 
 
     }
+
+    private void startLoginPage(LoginType phone) {
+
+        Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder builder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(phone,
+                AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                builder.build());
+        startActivityForResult(intent, REQUEST_CODE);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE){
+
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+
+            if (result.getError() != null){
+                Toast.makeText(this, ""+result.getError().getErrorType().getMessage(), Toast.LENGTH_LONG).show();
+            }else if (result.wasCancelled()){
+                Toast.makeText(this, "Cancel", Toast.LENGTH_LONG).show();
+            }else {
+                if (result.getAccessToken() != null){
+
+                    SpotsDialog alertDialog = new SpotsDialog(MainActivity.this);
+                    alertDialog.show();
+                    alertDialog.setMessage("Please waiting...");
+
+                    // get user phone
+                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                        @Override
+                        public void onSuccess(Account account) {
+
+                        }
+
+                        @Override
+                        public void onError(AccountKitError accountKitError) {
+
+                        }
+                    });
+
+
+                }
+            }
+
+        }
+    }
+
+
 
     private void printKeyHash() {
 
@@ -67,3 +144,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+// 2 // 10
